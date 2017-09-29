3d11d82869402749892bdc3aa94996d412120fd4
Harry Lachenmayer <harrylachenmayer@gmail.com>
2017-09-29 18:44:32 +0100

    wasm needs fs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const hypercore = require('hypercore')
+const pump = require('pump')
+const ram = require('random-access-memory')
+const signalhub = require('signalhub')
+const webrtcSwarm = require('webrtc-swarm')
+
+const feed = hypercore(file => ram(), {valueEncoding: 'json'})
+feed.ready(() => {
+  const hub = signalhub('datter/' + feed.key.toString('hex'), 'localhost:1337')
+  const swarm = webrtcSwarm(hub)
+  const peer = feed.replicate()
+  let peerCount = 0
+  swarm.on('peer', connection => {
+    console.log('CONNECT', peerCount++, 'peers for', feed.key.toString('hex'))
+    pump(peer, connection, peer, () => {
+      console.log('DISCONNECT:', peerCount--, 'peers for', feed.key.toString('hex'))
+    })
+  })
+})
 
 class App extends Component<void, void> {
   render() {
